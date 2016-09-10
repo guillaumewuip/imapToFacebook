4227bc279a1bb5a7fa2a9dfe3a2009844884ac36
Guillaume Wuip <guillaume@wuips.com>
2016-09-10 19:01:46 +0200

    Remove log

--- utils/parser.js
+++ utils/parser.js
@@ -7,15 +7,12 @@ const MailParser = require('mailparser').MailParser;
  * Read an email to extract fields like From, To, Content, etc.
  *
  * @param  {Object}     mailToParse
- * @param  {String}     mailToParse.uid     Mail uid
  * @param  {Buffer}     mailToParse.buffer  The mail
  *
  * @return {Promise}
  */
 const parseMail = (mailToParse) => {
     const mailparser = new MailParser();
-
-    console.log(`Will parse mail ${mailToParse.uid}`);
 
     return new Promise((resolve, reject) => {
         mailparser.on('end', (mail) => {
