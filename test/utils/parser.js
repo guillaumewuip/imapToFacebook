c65e0f68b83f3b16af05e6b37574726cc4914bf7
Guillaume Wuip <guillaume@wuips.com>
2016-09-10 08:38:19 +0200

    Add test of date

--- test/utils/parser.js
+++ test/utils/parser.js
@@ -15,11 +15,13 @@ describe('parser', () => {
             from:    [{ address: 'sender@example.com', name: 'Sender' }],
             to:      [{ address: 'receiver@example.com', name: 'Receiver' }],
             subject: 'Hello world!',
+            date:    new Date('Sat, 10 Sep 2016 08:28:39 +0200 (CEST)'),
         };
 
         const email = `From: Sender <sender@example.com>
 To: Receiver <receiver@example.com>
 Subject: Hello world!
+Date: Sat, 10 Sep 2016 08:28:39 +0200 (CEST)
 
 How are you today ?`;
 
@@ -29,6 +31,8 @@ How are you today ?`;
                 expect(result.subject).to.equal(data.subject);
                 expect(result.from).to.deep.equal(data.from);
                 expect(result.to).to.deep.equal(data.to);
+                expect(result.date.getTime())
+                    .to.deep.equal(data.date.getTime());
 
                 done();
             })
